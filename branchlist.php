b1a7e0b3f024d5e79ecc6c41d55e8af317f26197
Josh2609 <jcorps@gmx.com>
2016-11-11 00:34:14 +0000

    Fixed bug where user was logged out on accessing the branchlist page

--- branchlist.php
+++ branchlist.php
@@ -13,17 +13,59 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="Style.css" />
+        <script src="dropdown.js"></script>
         <title>Branch List</title>
     </head>
     <body>
         <div class="nav">
             <ul>
                 <li style="float:left; color:#999999"><a href="index.php">Carkea</a></li>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="#">Search</a></li>
+                <li><a class = "active" href="index.php">Home</a></li>
+                <li><a href="search.php">Search</a></li>
 		<li><a href="#">Contact Us</a></li>
-                <li><a class = "active" href="branchlist.php">Branch List</a></li>
+                <li><a href="branchlist.php">Branch List</a></li>
+                <?php 
+                if (isset($_SESSION['username'])) {
+                    $loggedInUser = $_SESSION["username"];
+                }
+                //session_destroy(); 
+                if (isset($_SESSION['loggedIn'])) {
+                    if($_SESSION['loggedIn'] == "true" )
+                    {   ?>
+                        <li class="dropdown">
+                        <button onclick="myFunction()" class="dropbtn"><?=$loggedInUser?></button>
+                        <div id="myDropdown" class="dropdown-content">
+                            <?php if ($_SESSION['staff'] === "false")
+                            {?>
+                                <a href="user/editProfile.php?id=<?=$_SESSION['customerID']?>">Update Details</a>
+                                <a href="user/updateaddress.php?id=<?=$_SESSION['customerID']?>">Update Addresses</a> <!-- Add if for user type **EDIT** -->
+                                <a href="#">View Purchases</a>
+                            <?php } else {?>
+                                <a href="staff/editProfile.php">Update Details</a>
+                                <a href="#">View Purchases</a> <!-- Add if for user type **EDIT** -->
+                                <a href="#">Link 3</a>
+                            <?php } ?>
+                        </div>
+                        </li>
+                        <li><a href="php_files/Logout.php">Logout</a></li>
+                    <?php } else { ?>
+                    <li><a href="login.php">Login</a></li>
+                    <?php } 
+                } else { ?>
                 <li><a href="login.php">Login</a></li>
+                <?php }
+
+                if (isset($_SESSION["accessLevel"])) 
+                {
+                    if($_SESSION["accessLevel"] == "1")
+                    {?>
+                        <li><a href="staff/addstock.php">Add Stock</a></li>
+                    <?php }  
+                    else if($_SESSION["accessLevel"] == "3")
+                    {?>
+                        <li><a href="staff/addemployee.php">Add Employee</a></li>
+                    <?php }  
+                }?>
             </ul>
         </div> <!-- nav close -->
         <div class="mainbody">
