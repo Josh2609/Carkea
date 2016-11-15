7d374e8bd69a7e6fa70b89e646a7729c29dc3da2
Josh2609 <jcorps@gmx.com>
2016-11-15 01:09:24 +0000

    added mark as sold option, needs testing

---
+++ php_files/markSold.php
@@ -0,0 +1,60 @@
+<?php
+
+    $soldPrice = $_POST["soldPrice"];
+    $soldCustID = $_POST["soldCustID"];
+    $vin = $_POST["vin"];
+    $branchID = $_POST["branchID"];
+    $carStockID = $_POST["carStockID"];
+    if (isset($_POST["paymentMethod"]))
+    {
+        $yesFinance = 'true';
+        $paymentMethod = $_POST["paymentMethod"];
+        $financeAmount = $_POST["financeAmount"];
+        $monthlyPayments = $_POST["monthlyPayments"];
+        $fCompanyID = $_POST["fCompanySelect"];
+    }
+
+    $dbConnection = new PDO('mysql:dbname=16ac3d07;host=silva.computing.dundee.ac.uk;charset=utf8', '16ac3u07', 'bac132');
+
+    $dbConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    $stmt = $dbConnection->prepare('INSERT INTO SoldCar '
+             . '(Sold_Car_ID, Sold_Price, Sold_Date, Vehicle_Identification_Number, Customer_ID, Branch_ID)'
+             . 'VALUES (NULL, :soldPrice, CURDATE(), :vin, :custID, :branchID)');
+     
+    $stmt->bindParam(':soldPrice', $soldPrice);
+    $stmt->bindParam(':vin', $vin);
+    $stmt->bindParam(':custID', $soldCustID);
+    $stmt->bindParam(':branchID', $branchID);
+    
+    $lastID = $dbConnection->lastInsertId();
+    
+    if ($stmt->execute())
+    {
+        if ($yesFinance = 'true')
+        {
+            $stmt = $dbConnection->prepare('INSERT INTO FinancePlan '
+                    . '(Finance_ID, Payment_Method, Monthly_Repayment_Amount, Total_Finance_Amount, Outstanding_Payment_Remaining, Finance_Company_ID, Sold_Car_ID)'
+                    . 'VALUES(NULL, :paymentMethod, :monthlyPayments, :financeAmount, :outstandingPayment, :companyID, :soldCarID)');
+            $stmt->bindParam(':paymentMethod', $paymentMethod);
+            $stmt->bindParam(':monthlyPayments', $monthlyPayments);
+            $stmt->bindParam(':financeAmount', $financeAmount);
+            $stmt->bindParam(':outstandingPayment', $financeAmount);
+            $stmt->bindParam(':companyID', $fCompanyID);
+            $stmt->bindParam(':soldCarID', $lastID);
+
+            $stmt->execute();
+        }
+        
+        $stmt = $dbConnection->prepare('DELETE FROM CarStock WHERE Car_Stock_ID=:carStockID');
+        $stmt->bindParam(':carStockID', $carStockID);
+        
+        if ($stmt->execute())
+        {
+             header("Location: ../stock.php?id=$vin");
+        }
+    }
+    
+    // **EDIT**
+    
--- stock.php
+++ stock.php
@@ -27,8 +27,19 @@ th {text-align: left;}
 </style>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="newStyle.css" />
-        <script src="dropdown.js"></script>
+        
         <script src="js/showCarDetails.js"></script>
+        <script>
+            function showFinanceForm()
+            {
+                document.getElementById("financeDiv").style.display = "block";
+                document.getElementById("btnFinance").style.visibility = "hidden";
+            }       
+            function getFinanceCompanyID(option) {
+                fCompanyID = option.value;  
+            }
+        </script>
+        
         <title>Carkea</title>
     </head>
     <body>
@@ -129,13 +140,57 @@ th {text-align: left;}
                     <h5>Branches where ***DO DIS***this car is Available</h5>
                     </div> ';
                 
-               if (isset($_SESSION["accessLevel"])) 
+                $branchID = $row["Branch_ID"];
+                $carStockID = $row["Car_Stock_ID"];
+                
+                if (isset($_SESSION["accessLevel"])) 
                 {
                     if($_SESSION["accessLevel"] == "1" || $_SESSION["accessLevel"] == "2")
                     {?>
                         <div class="wishList">
-                            <button id="wishList">Mark as Sold</button>
+                            <button id="markSold">Mark as Sold</button>
                         </div>
+               
+                        <div id="soldModal" class="modal">
+                        <!-- Modal content -->
+                            <div class="modal-content">
+                                <span class="close">×</span>
+                                <form method="POST"  action="php_files/markSold.php">
+                                    <ul style='list-style:none;'>
+                                        <li>Price sold for<input type="text" name="soldPrice"></li>
+                                        <li><br></li>
+                                        <li>ID of Customer<input type="text" name="soldCustID"></li>
+                                        <li><br></li>
+                                    </ul>
+                                    <input type="hidden" name="vin" value="<?php echo $vin ?>">
+                                    <input type="hidden" name="branchID" value="<?php echo $branchID ?>">
+                                    <input type="hidden" name="carStockID" value="<?php echo $carStockID ?>">
+                                    
+                                    <div style="display:none" id="financeDiv">
+                                        <ul style='list-style:none;'>
+                                            <li>Payment Method<input type="text" name="paymentMethod"></li>
+                                            <li>Finance Amount<input type="text" name="financeAmount"></li>
+                                            <li>Monthly Payments<input type="text" name="monthlyPayments"></li>
+                              
+                                            <li><select id="fCompanySelect" name="fCompanySelect" onchange="getFinanceCompanyID(this)" style="width: 155px;height:30px;color:#2d5986">
+                                            <option value="">Finance Company</option>
+                                            <?php
+                                            $stmt2 = $dbConnection->prepare("SELECT Finance_Company_ID, Company_Name FROM FinanceCompany");  
+                                            $stmt2->execute();
+                                            foreach ($stmt2 as $data)
+                                            { ?>
+                                                <option value="<?=$data['Company_ID'];?>"><?=$data['Finance_Company_Name'];?></option>
+                                            <?php } ?>
+                                            </select></li>
+                                        </ul>
+                                    </div>
+                                        <button type="button" id="btnFinance" onclick="showFinanceForm()">Add Finance?</button> 
+                                    <input type="submit" value="Submit"> 
+                                </form>
+                            </div>
+                        </div>
+                    <script src="js/markSoldModal.js"></script>
+
                     <?php } else {?>
                         
                     <?php }  
